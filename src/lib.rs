725db8810c447de9b6ad886d75b20e9f65c24adb
brkp <brkp@tutanota.com>
2021-12-23 20:49:59 +0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
