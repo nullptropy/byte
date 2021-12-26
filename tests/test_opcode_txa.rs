d1ff96ccc4939f7068113c0a21b550ab50e35b66
brkp <brkp@tutanota.com>
2021-12-26 15:53:14 +0300

    delete old incomplete tests

--- tests/test_opcode_txa.rs
+++
@@ -1,9 +0,0 @@
-use mos6502::*;
-
-#[test]
-fn opcode_0xaa_implied_tax() {
-    let mut cpu = cpu::CPU::new();
-    cpu.bus.attach(0x0000, 0xffff, bus::MockRAM::new(0x10000)).unwrap();
-
-    assert_eq!(2 + 2, 5);
-}
