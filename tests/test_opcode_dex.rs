69508697f5d1dbb03d12c8bd3d4f837ac020c5f7
brkp <brkp@tutanota.com>
2021-12-27 09:06:56 +0300

    comments

--- tests/test_opcode_dex.rs
+++ tests/test_opcode_dex.rs
@@ -6,9 +6,9 @@ fn opcode_0xca_implied_dex() {
 
     cpu.reg.x = 0x5;
     cpu.load_and_run(&[
-        0xca,
-        0xca,
-        0x00,
+        0xca,   // DEX
+        0xca,   // DEX
+        0x00,   // BRK
     ], 0x8000);
 
     assert_eq!(cpu.reg.x, 0x3);
