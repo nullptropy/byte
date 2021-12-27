9708f9553a4e44ad4fddda867a1f382fd67ceeb5
brkp <brkp@tutanota.com>
2021-12-27 09:06:17 +0300

    test case for the inx instruction

--- tests/test_opcode_inx.rs
+++ tests/test_opcode_inx.rs
@@ -2,6 +2,16 @@ mod common;
 
 #[test]
 fn opcode_0xe8_implied_inx() {
-    let mut _cpu = common::init_cpu();
-    assert_eq!(2 + 2, 5);
+    let mut cpu = common::init_cpu();
+
+    cpu.load_and_run(&[
+        0xe8,   // INX
+        0xe8,   // INX
+        0xe8,   // INX
+        0xe8,   // INX
+        0xe8,   // INX
+        0x00,   // BRK
+    ], 0x8000);
+
+    assert_eq!(cpu.reg.x, 0x5);
 }
