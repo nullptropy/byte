179e70e337aaabf99669154e6f8ec41806a5201f
brkp <brkp@tutanota.com>
2021-12-27 09:36:34 +0300

    test case for tax instruction

--- tests/test_opcode_tax.rs
+++ tests/test_opcode_tax.rs
@@ -2,6 +2,14 @@ mod common;
 
 #[test]
 fn opcode_0xaa_implied_tax() {
-    let mut _cpu = common::init_cpu();
-    assert_eq!(2 + 2, 5);
+    let mut cpu = common::init_cpu();
+
+    cpu.reg.a = 0x5;
+    cpu.load_and_run(&[
+        0xaa,   // TAX
+        0x00,   // BRK
+    ], 0x8000);
+
+    assert_eq!(cpu.reg.x, 0x5);
+    assert_eq!(cpu.reg.p.bits(), 0x00);
 }
