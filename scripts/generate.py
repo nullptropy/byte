805104dcea957dbf40b9ecdb908e50ad7d6517f4
brkp <brkp@tutanota.com>
2022-01-07 18:42:27 +0300

    new test format

--- scripts/generate.py
+++ scripts/generate.py
@@ -5,31 +5,27 @@ import os
 import sys
 import json
 
-TEST_FILE_HEADER = 'mod common;\n\n'
+
+GET_PATH = lambda *p: os.path.join(os.path.dirname(sys.argv[0]), '..', *p)
+
 TEST_FUNC_TEMPLATE = '''\
 #[test]
 fn opcode_0x{code}_{mode}_{name}() {{
-    let mut _cpu = common::init_cpu();
+    let cpu = execute_nsteps(|_| {{}}, &[], 0x8000, 0);
     assert_eq!(2 + 2, 5);
 }}\n'''
 
 OPCODE_TEMPLATE = '(0x{code}, Opcode::new(0x{code}, {size}, {tick}, "{name}", AddressingMode::{mode}, None)),'
 _MATCH_TEMPLATE = '{codes} => self.{name}(&opcode)'
 
-GET_PATH = lambda *p: os.path.join(os.path.dirname(sys.argv[0]), '..', *p)
-
 
 def generate_test_file(instructions):
     print(
-        TEST_FILE_HEADER +
         '\n'.join(map(
             lambda ins: TEST_FUNC_TEMPLATE.format(
                 code=ins['code'],
                 mode=ins['mode'].lower(),
-                name=ins['name'].lower()), instructions)).rstrip(),
-        file=open(GET_PATH('tests', f'test_opcode_{instructions[0]["name"].lower()}.rs'), 'w'),
-        end='')
-
+                name=ins['name'].lower()), instructions)).rstrip(), end='\n\n')
 
 def generate_code_file(instructions):
     print('\n'.join(
@@ -46,8 +42,7 @@ def main(argc, argv):
             return print(f'{argv[0]} [instructions.json] OPCODE') or 1
 
     instructions = list(filter(
-        lambda op: op['name'] == opcode.upper(),
-        json.load(open(instructions, 'r'))))
+        lambda op: op['name'] == opcode.upper(), json.load(open(instructions, 'r'))))
 
     generate_test_file(instructions)
     generate_code_file(instructions)
