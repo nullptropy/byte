2419292aec49b3a55647b1df487433b54d9123df
brkp <brkp@tutanota.com>
2022-01-15 14:43:13 +0300

    prelude

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,17 @@
 pub mod cpu;
 pub mod bus;
 pub mod opcode;
+
+pub mod prelude {
+    pub use crate::bus::{Bus, Peripheral};
+
+    pub use crate::cpu::{
+        CPU, Flags, Interrupt,
+        NMI_VECTOR, RST_VECTOR, IRQ_VECTOR,
+    };
+
+    pub use crate::opcode::{
+        Opcode, OPCODE_MAP, Operand,
+        AddressingMode, TickModifier,
+    };
+}
