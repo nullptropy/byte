4f2ec81e22c9cdbd8c28fc989055f2f7652b985b
brkp <brkp@tutanota.com>
2023-02-08 00:52:53 +0300

    clean up the bus implementation

--- src/bus.rs
+++ src/bus.rs
@@ -1,85 +1,94 @@
-use std::ops::Range;
-use std::ops::ControlFlow;
-
 pub trait Peripheral {
     fn read(&self, addr: u16) -> u8;
     fn write(&mut self, addr: u16, byte: u8);
 }
 
-// TODO: implement the `Bus` struct in some other way
-// that does not suck
+struct PeripheralItem {
+    range: (u16, u16),
+    peripheral: Box<dyn Peripheral>,
+}
+
 #[derive(Default)]
 pub struct Bus {
-    peripherals: Vec<(Range<usize>, Box<dyn Peripheral>)>,
+    peripherals: Vec<PeripheralItem>,
+}
+
+impl PeripheralItem {
+    fn new(range: (u16, u16), peripheral: Box<dyn Peripheral>) -> Self {
+        Self { range, peripheral }
+    }
+
+    fn handles(&self, addr: u16) -> bool {
+        self.range.0 <= addr && self.range.1 >= addr
+    }
+
+    fn overlaps(&self, range: (u16, u16)) -> bool {
+        range.0 < self.range.1 && range.1 > self.range.0
+    }
 }
 
 impl Bus {
     pub fn read(&self, addr: u16) -> u8 {
-        match self.get_peripheral_index(addr) {
-            Some((addr, index)) => self.peripherals[index].1.read(addr),
-            _ => 0
+        if let Some((i, addr)) = self.get_peripheral_index(addr) {
+            self.peripherals[i].peripheral.read(addr)
+        } else {
+            0
         }
     }
 
     pub fn write(&mut self, addr: u16, byte: u8) {
-        if let Some((addr, index)) = self.get_peripheral_index(addr) {
-            self.peripherals[index].1.write(addr, byte);
+        if let Some((i, addr)) = self.get_peripheral_index(addr) {
+            self.peripherals[i].peripheral.write(addr, byte);
         }
     }
 
     pub fn read_u16(&self, addr: u16) -> u16 {
-        match self.get_peripheral_index(addr) {
-            Some((addr, index)) => (
-                self.peripherals[index].1.read(addr + 1) as u16) << 8 |
-                self.peripherals[index].1.read(addr) as u16,
-            _ => 0
+        if let Some((i, addr)) = self.get_peripheral_index(addr) {
+            let hi = self.peripherals[i].peripheral.read(addr + 1);
+            let lo = self.peripherals[i].peripheral.read(addr);
+
+            (hi as u16) << 8 | (lo as u16)
+        } else {
+            0
         }
     }
 
     pub fn write_u16(&mut self, addr: u16, data: u16) {
-        if let Some((addr, index)) = self.get_peripheral_index(addr) {
-            self.peripherals[index].1.write(addr + 0, (data & 0xff) as u8); // low byte
-            self.peripherals[index].1.write(addr + 1, (data >>   8) as u8); // high byte
+        if let Some((i, addr)) = self.get_peripheral_index(addr) {
+            self.peripherals[i]
+                .peripheral
+                .write(addr, (data & 0xff) as u8); // low byte
+            self.peripherals[i]
+                .peripheral
+                .write(addr + 1, (data >> 8) as u8); // high byte
         }
     }
 
-    pub fn get_peripheral_index(&self, addr: u16) -> Option<(u16, usize)> {
-        let result = self.peripherals.iter().enumerate().try_for_each(|(i, (range, _))| {
-            if range.contains(&(addr as usize)) {
-                return ControlFlow::Break((addr - range.start as u16, i));
+    pub fn get_peripheral_index(&self, addr: u16) -> Option<(usize, u16)> {
+        for (i, peripheral) in self.peripherals.iter().enumerate() {
+            if peripheral.handles(addr) {
+                return Some((i, addr - peripheral.range.0));
             }
-
-            ControlFlow::Continue(())
-        });
-
-        // TODO: refactor this with `.break_value` when it is
-        // stabilized, feature: control_flow_enum
-        if let ControlFlow::Break((addr, i)) = result {
-            return Some((addr, i));
         }
 
         None
     }
 
-    pub fn attach<T>(&mut self, lo: u16, hi: u16, peripheral: T) -> Result<(), String>
+    pub fn attach<P>(&mut self, lo: u16, hi: u16, peripheral: P) -> Result<(), String>
     where
-        T: Peripheral + 'static,
+        P: Peripheral + 'static,
     {
-        let lo = lo as usize;
-        let hi = hi as usize;
-
-        self.peripherals.iter().try_for_each(|(range, _)| {
-            if lo < range.end && hi > range.start {
+        for item in self.peripherals.iter() {
+            if item.overlaps((lo, hi)) {
                 return Err(format!(
                     "overlapping ranges: [{:x}:{:x}] and [{:x}:{:x}]",
-                    lo, hi,
-                    range.start, range.end - 1
+                    lo, hi, item.range.0, item.range.1,
                 ));
             }
+        }
 
-            Ok(())
-        })?;
-
-        Ok({ self.peripherals.push((lo..hi + 1, Box::new(peripheral))); })
+        self.peripherals
+            .push(PeripheralItem::new((lo, hi), Box::new(peripheral)));
+        Ok(())
     }
 }
