7071460c6b6f4d732d34443c0b0d8af8cd183d44
brkp <brkp@tutanota.com>
2023-02-25 18:28:07 +0300

    fmt

--- byte_emu/assets/static.s
+++ byte_emu/assets/static.s
@@ -6,47 +6,47 @@ RANDOM EQU $fe
 INPUT  EQU $ff
 
 reset:
-	ldx #$ff
-	txs			; initialize the stack pointer
-	lda #$1
-	sta VIDEO	; set the video page to $1000
+    ldx #$ff
+    txs         ; initialize the stack pointer
+    lda #$1
+    sta VIDEO   ; set the video page to $1000
 
-	jsr init
-	jmp loop
+    jsr init
+    jmp loop
 
 init:
-	lda #$00
-	sta $10		; low byte of the counter
-	lda #$10
-	sta $11		; high byte of the counter
-	rts
+    lda #$00
+    sta $10     ; low byte of the counter
+    lda #$10
+    sta $11     ; high byte of the counter
+    rts
 
 loop:
-	jsr draw
-	jsr update
-	jmp loop
+    jsr draw
+    jsr update
+    jmp loop
 
 draw:
-	ldy #$00
-	lda RANDOM
-	sta ($10), y
-	rts
+    ldy #$00
+    lda RANDOM
+    sta ($10), y
+    rts
 
 update:
-	lda $10
-	clc
-	adc #$01
-	sta $10
-	bcc ret		; if the carry bit is clear, return
-	lda $11
-	clc
-	adc #$01
-	sta $11
-	cmp #$20
-	bne ret		; if the counter has reached to $2000, re-init the counter
-	jsr init
+    lda $10
+    clc
+    adc #$01
+    sta $10
+    bcc ret     ; if the carry bit is clear, return
+    lda $11
+    clc
+    adc #$01
+    sta $11
+    cmp #$20
+    bne ret     ; if the counter has reached to $2000, re-init the counter
+    jsr init
 ret:
-	rts
+    rts
 
 .org $fffc
     .DW reset
