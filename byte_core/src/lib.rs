e3d7a7af10c7615cf0d847f348d6859b8f7e1341
brkp <brkp@tutanota.com>
2023-02-28 21:48:25 +0300

    custom error type

--- byte_core/src/lib.rs
+++ byte_core/src/lib.rs
@@ -3,3 +3,18 @@
 pub mod bus;
 pub mod cpu;
 pub mod opcode;
+
+#[derive(Debug)]
+pub enum Error {
+    UnrecognizedOpcode(u8),
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::UnrecognizedOpcode(code) => {
+                write!(f, "Unrecognized Opcode: {code:#04X}")
+            }
+        }
+    }
+}
