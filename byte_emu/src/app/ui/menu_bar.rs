aae642fa7a4d45832bfbc3ecab7a014e5168ee2a
brkp <brkp@tutanota.com>
2023-03-04 01:12:41 +0300

    fmt

--- byte_emu/src/app/ui/menu_bar.rs
+++ byte_emu/src/app/ui/menu_bar.rs
@@ -9,47 +9,54 @@ impl ByteEmuApp {
 
     fn ui_menu_bar(&mut self, ui: &mut egui::Ui) {
         egui::menu::bar(ui, |ui| {
-            ui.menu_button("File", |ui| {
-                use FileProcesserMessage::*;
-
-                if ui.button("Load binary program").clicked() {
-                    self.file_processer
-                        .read(|name, data| BinaryFile((name, data)));
-                    ui.close_menu();
-                }
-                if ui.button("Load source file").clicked() {
-                    self.file_processer
-                        .read(|name, data| SourceFile((name, data)));
-                    ui.close_menu();
-                }
-
-                ui.separator();
-
-                if ui.button("Reset GUI state").clicked() {
-                    ui.ctx().memory_mut(|mem| *mem = Default::default());
-                    ui.close_menu();
-                }
-                if ui.button("Reset everything").clicked() {
-                    self.state = State::default();
-                    ui.ctx().memory_mut(|mem| *mem = Default::default());
-                    ui.close_menu();
-                }
-            });
-
-            ui.menu_button("Tools", |ui| {
-                if ui.button("Code Editor").clicked() {
-                    self.state.is_code_editor_open = true;
-                    ui.close_menu();
-                }
-
-                if ui.button("Frame History").clicked() {
-                    self.state.is_frame_history_open = true;
-                    ui.close_menu();
-                }
-            });
+            self.ui_file_button(ui);
+            self.ui_tools_button(ui);
 
             if ui.button("About").clicked() {
                 self.state.is_about_open = true;
+            }
+        });
+    }
+
+    fn ui_file_button(&mut self, ui: &mut egui::Ui) {
+        ui.menu_button("File", |ui| {
+            use FileProcesserMessage::*;
+
+            if ui.button("Load binary program").clicked() {
+                self.file_processer
+                    .read(|name, data| BinaryFile((name, data)));
+                ui.close_menu();
+            }
+            if ui.button("Load source file").clicked() {
+                self.file_processer
+                    .read(|name, data| SourceFile((name, data)));
+                ui.close_menu();
+            }
+
+            ui.separator();
+
+            if ui.button("Reset GUI state").clicked() {
+                ui.ctx().memory_mut(|mem| *mem = Default::default());
+                ui.close_menu();
+            }
+            if ui.button("Reset everything").clicked() {
+                self.state = State::default();
+                ui.ctx().memory_mut(|mem| *mem = Default::default());
+                ui.close_menu();
+            }
+        });
+    }
+
+    fn ui_tools_button(&mut self, ui: &mut egui::Ui) {
+        ui.menu_button("Tools", |ui| {
+            if ui.button("Code Editor").clicked() {
+                self.state.is_code_editor_open = true;
+                ui.close_menu();
+            }
+
+            if ui.button("Frame History").clicked() {
+                self.state.is_frame_history_open = true;
+                ui.close_menu();
             }
         });
     }
