452ee0f07bf5193670e4b9c4efebea01c4377f99
brkp <brkp@tutanota.com>
2023-03-11 17:08:08 +0300

    fix horizontal scrolling issue in the memory monitor

--- byte_emu/src/app/ui/memory_monitor.rs
+++ byte_emu/src/app/ui/memory_monitor.rs
@@ -5,6 +5,7 @@ impl ByteEmuApp {
         let mut open = self.state.is_memory_monitor_open;
         egui::Window::new("Memory Monitor")
             .open(&mut open)
+            .default_width(590.0)
             .show(ctx, |ui| {
                 self.ui_memory_monitor(ui);
             });
@@ -15,20 +16,16 @@ impl ByteEmuApp {
         let addr_str = &mut self.state.memory_window_range_str.0;
         let size_str = &mut self.state.memory_window_range_str.1;
 
-        ui.scope(|ui| {
-            ui.style_mut().override_font_id = Some(egui::FontId::monospace(14.0));
-
-            ui.horizontal(|ui| {
-                ui.label("addr:");
-                ui.text_edit_singleline(addr_str);
-            });
-            ui.horizontal(|ui| {
-                ui.label("size:");
-                ui.text_edit_singleline(size_str);
-            });
-
-            ui.add_space(14.0);
+        ui.style_mut().override_font_id = Some(egui::FontId::monospace(14.0));
+        ui.horizontal(|ui| {
+            ui.label("addr:");
+            ui.text_edit_singleline(addr_str);
         });
+        ui.horizontal(|ui| {
+            ui.label("size:");
+            ui.text_edit_singleline(size_str);
+        });
+        ui.add_space(14.0);
 
         if let (Ok(start), Ok(end)) = (
             u16::from_str_radix(addr_str.trim_start_matches("0x"), 16),
@@ -41,34 +38,29 @@ impl ByteEmuApp {
         let mem_slice = self.emu.get_memory_region(self.state.memory_window_range);
         self.state.memory_window_text_area.clear();
 
-        mem_slice.chunks(16).for_each(|chunk| {
-            let ascii = format!(
-                "{: <16}",
-                chunk
-                    .iter()
-                    .map(|b| match b {
-                        0x20..=0x7e => *b as char,
-                        _ => '.',
-                    })
-                    .collect::<String>()
-            );
-            let bytes = format!("{chunk:02x?}").replace(',', "");
-            let output = format!(
-                "{counter:04x}: {: <47} |{ascii}|\n",
-                &bytes[1..bytes.len() - 1]
-            );
-            self.state.memory_window_text_area.push_str(output.as_str());
+        egui::ScrollArea::both().show(ui, |ui| {
+            ui.vertical(|ui| {
+                mem_slice.chunks(16).for_each(|chunk| {
+                    let ascii = format!(
+                        "{: <16}",
+                        chunk
+                            .iter()
+                            .map(|b| match b {
+                                0x20..=0x7e => *b as char,
+                                _ => '.',
+                            })
+                            .collect::<String>()
+                    );
+                    let bytes = format!("{chunk:02x?}").replace(',', "");
+                    let output = format!(
+                        "{counter:04x}: {: <47} |{ascii}|",
+                        &bytes[1..bytes.len() - 1]
+                    );
+                    ui.add(egui::Label::new(output).wrap(false));
 
-            counter += chunk.len() as u16;
-        });
-
-        egui::ScrollArea::vertical().show(ui, |ui| {
-            ui.add_sized(
-                ui.available_size(),
-                egui::TextEdit::multiline(&mut self.state.memory_window_text_area)
-                    .code_editor()
-                    .desired_width(f32::INFINITY),
-            );
+                    counter += chunk.len() as u16;
+                });
+            });
         });
     }
 }
