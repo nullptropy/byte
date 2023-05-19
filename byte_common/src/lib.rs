4791e7508678476fb2aa501bb0f980641eee5121
brkp <brkp@tutanota.com>
2023-05-19 16:04:50 +0300

    `get_opcode` function

--- byte_common/src/lib.rs
+++ byte_common/src/lib.rs
@@ -4,4 +4,11 @@ pub mod opcode {
     pub use super::opcode_types::*;
     pub const OPCODE_MAP: [Option<Opcode>; 255] =
         include!(concat!(env!("OUT_DIR"), "/opcode_arr.rs"));
+
+    pub fn get_opcode(name: &str) -> Option<&Opcode> {
+        OPCODE_MAP
+            .iter()
+            .flatten()
+            .find(|opcode| opcode.name.eq_ignore_ascii_case(name))
+    }
 }
