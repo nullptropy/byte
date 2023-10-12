6c6ac57a032270fbc4229d3085d011ff87033f4d
brkp <brkp@tutanota.com>
2023-10-12 21:10:04 +0300

    asm: nuke the existing implementation
    Signed-off-by: brkp <brkp@tutanota.com>

--- byte_asm/src/lex/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod error;
-pub mod lexer;
-pub mod token;
-
-pub use error::LexerError;
-pub use lexer::Lexer;
-pub use token::{Location, Token, TokenLiteral, TokenType};
-
-pub type LexerResult<T> = Result<T, error::LexerError>;
--- byte_asm/src/main.rs
+++ byte_asm/src/main.rs
@@ -1,18 +1,1 @@
-use byte_asm::lex::Lexer;
-
-fn main() {
-    let path = std::env::args().nth(1).unwrap_or("test.s".to_string());
-    let source = std::fs::read_to_string(path).expect("failed to read the file");
-    let mut lexer = Lexer::new(&source);
-    let mut tokens = Vec::new();
-
-    loop {
-        match lexer.scan_token() {
-            Ok(token) if token.eof() => break,
-            Ok(token) => tokens.push(token),
-            Err(why) => println!("syntax error:\n    {why:?}\n    {why}"),
-        };
-    }
-
-    println!("{tokens:#?}");
-}
+fn main() {}
