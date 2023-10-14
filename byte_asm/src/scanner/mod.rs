d96138b18ac078f53327c56d3d1a523791416145
brkp <brkp@tutanota.com>
2023-10-15 01:40:25 +0300

    asm: scanner: clippy fixes

--- byte_asm/src/scanner/mod.rs
+++ byte_asm/src/scanner/mod.rs
@@ -1,10 +1,10 @@
 mod cursor;
 pub mod error;
-pub mod scanner;
+pub mod scan;
 pub mod token;
 
 pub use error::ScannerError;
-pub use scanner::Scanner;
+pub use scan::Scanner;
 pub use token::{Directive, Location, Token, TokenKind, TokenValue};
 
 pub type ScannerResult<T> = std::result::Result<T, ScannerError>;
